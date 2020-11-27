afa83fba2453d69485baf012a7b05f9224e62d3e
John Cherry <github@johncherry.me>
2020-11-27 10:15:17 +0000

    List all made choices

---
+++ resources/js/components/casting/listCast.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="col">
+        <div class="card card-warning">
+            <div class="card-header">
+                <h3 class="card-title">Cast Roles</h3>
+            </div>
+            <div class="card-body">
+                <table class="table table-responsive-sm">
+                    <thead>
+                        <tr>
+                            <th>Role</th>
+                            <th>Option 1</th>
+                            <th>Option 2</th>
+                            <th>Option 3</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="choice in productionchoices">
+                            <td>{{ choice.role }}</td>
+                            <td>{{ choice.first }}</td>
+                            <td>{{ choice.second }}</td>
+                            <td>{{ choice.third }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+    props:{
+        productionchoices:{
+            type: Object,
+        },
+    }
+}
+</script>
--- resources/views/user/choices.blade.php
+++ resources/views/user/choices.blade.php
@@ -2,4 +2,9 @@
 
 @section('content')
 
+<div id="app">
+    <list-cast v-bind:productionChoices="{{ json_encode($productionChoices) }}">
+    <p>hello</p>
+</div>
+<script src="{{ asset('js/app.js') }}" defer></script>
 @endsection
