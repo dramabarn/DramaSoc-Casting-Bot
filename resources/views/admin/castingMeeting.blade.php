62fab68f2dc05b1a094e5f75e46569e8bd256e77
Nathan Billis <nathanbillis.nathan@gmail.com>
2020-11-28 22:41:51 +0000

    switch conflicts and deadlocks around

--- resources/views/admin/castingMeeting.blade.php
+++ resources/views/admin/castingMeeting.blade.php
@@ -5,8 +5,8 @@
     <div id="app">
         <div class="col">
             <free-to-cast v-bind:castable="{{ json_encode($freeCast ?? '') }}"></free-to-cast>
-            <list-deadlocks v-bind:deadlocks="{{ json_encode($deadlocks ?? '' ) }}"></list-deadlocks>
             <view-conflicts v-bind:productionconflicts="{{ json_encode($sharing ?? '' ) }}"></view-conflicts>
+            <list-deadlocks v-bind:deadlocks="{{ json_encode($deadlocks ?? '' ) }}"></list-deadlocks>
             <view-remaining-cast v-bind:productionchoices="{{ json_encode($productionchoices ?? '') }}"></view-remaining-cast>
             <view-casted v-bind:casted="{{ json_encode($casted ?? '') }}"></view-casted>
         </div>
